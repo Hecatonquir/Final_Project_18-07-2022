45b8306c40ddb5317bc9b033b22240c06083af8f
JhonatanPG02 <jhonatan.prado.gutierrez@gmail.com>
2022-07-18 20:02:22 -0500

    Add EventCard component

---
+++ client/src/Components/EventCard.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Link from 'react-router-dom'
+
+export default function EventCard({id, image, name, date, price}) {
+    return(
+        <Link to={`/xxxxx/${id}`}> {/*Pendiente revision de ruta*/}
+        <div>
+            <img src={image} alt='img eventCard'/>
+            <h4>{name}</h4>
+            <p>{date}</p>
+            <p>{price}</p>
+        </div>
+        </Link>
+    )
+}
