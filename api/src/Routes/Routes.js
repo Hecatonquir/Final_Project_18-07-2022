66cb87252b88147db2f32ef45d60ef71608df651
SEIT0 <seito99@gmail.com>
2022-07-19 09:18:09 -0300

    add /allEvents route

--- api/src/Routes/Routes.js
+++ api/src/Routes/Routes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { Events, Users } = require('../db.js');
 
 const routes = express.Router();
 
@@ -8,27 +9,8 @@ const T = require('../Functions/Tickets');
 
 routes.use(express.json);
 
-/* Lo ideal sería hacer que haya una única ruta users y de ahi escribamos todos las sub rutas de users (lo mismo con las demás) */
-
-routes.get('/users', F.getUserByName);
-routes.post('/users', getUserByName);
-routes.put('/users', getUserByName);
-routes.delete('/users', getUserByName);
-
-routes.get('/events', getEventByName);
-routes.post('/events', newRecipe);
-routes.put('/events', newRecipe);
-routes.delete('/events', newRecipe);
-
-routes.get('/support/:idReceta', getSupportTickets);
-routes.post('/support/:idReceta', getSupportTickets);
-routes.put('/support/:idReceta', getSupportTickets);
-routes.delete('/support/:idReceta', getSupportTickets);
-
-/* Esto es para llamar todos los eventos apenas abro la Web App */
-routes.get('/allEvents', async (req, res, next) => {
-	let allEvents = await getAllEvents();
-	res.send(allEvents);
+routes.get('/allEvents', async (req, res) => {
+	res.send(await Events.findAll());
 });
 
 module.exports = routes;
