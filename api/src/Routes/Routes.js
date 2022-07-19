c9aa49dbd7722b979bcecd614e730f069ed72c06
SEIT0 <seito99@gmail.com>
2022-07-19 10:22:11 -0300

    add delete event route

--- api/src/Routes/Routes.js
+++ api/src/Routes/Routes.js
@@ -13,4 +13,14 @@ routes.get('/allEvents', async (req, res) => {
 	res.send(await Events.findAll());
 });
 
+routes.delete('/event/:id', async (req, res)=>{
+	try {
+		const trash = await Events.findByPk(req.params.id)
+		await trash.destroy()
+		res.send('Event Deleted.')
+	} catch (error) {
+		res.status(404).send(error)
+	}
+})
+
 module.exports = routes;
