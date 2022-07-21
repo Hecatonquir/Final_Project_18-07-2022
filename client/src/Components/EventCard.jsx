4b7af1caa7efbd35d9c8e9b50dee91daa58c42c9
Hecatonquir <maxicuentas92@hotmail.com>
2022-07-21 18:02:55 -0300

    Changed Api events examples to appear more realistic. Fixed Some details in the Events rendering in Home Component.

--- client/src/Components/EventCard.jsx
+++ client/src/Components/EventCard.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
 
-export default function EventCard({id, image, name, date, price,quantity,city,location}) {
+export default function EventCard({id, image, name, day, hour, price,quantity,city,location}) {
     return(
         <Link to={`/details/id/${id}`}>
         <div>
             <img src={image} alt='img eventCard'/>
             <h4>{name}</h4>
-            <p>Date: {date}</p>
+            <p>Date: {day} / {hour} </p>
             <p>Cost: {price}$</p>
             <p>Tickets Available: {quantity}</p>
             <p>City: {city}</p>
--- client/src/Components/Home.jsx
+++ client/src/Components/Home.jsx
@@ -1,49 +1,52 @@
-import {React, useEffect} from 'react'
-import {useDispatch,useSelector} from 'react-redux'
-import getEvents from '../Redux/Actions/getEvents.js'
-import ButtonFilter from './ButtonFilter.jsx'
-import EventCard from './EventCard.jsx'
-import NavBar from './NavBar.jsx'
-import Search from './Search.jsx'
-import EventCarousel from './Carousel.jsx'
-import Loader from './Loader.jsx'
-import CalendarEvents from './Calendar.jsx'
+import { React, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import getEvents from '../Redux/Actions/getEvents.js';
+import ButtonFilter from './ButtonFilter.jsx';
+import EventCard from './EventCard.jsx';
+import NavBar from './NavBar.jsx';
+import Search from './Search.jsx';
+import EventCarousel from './Carousel.jsx';
+import Loader from './Loader.jsx';
+import CalendarEvents from './Calendar.jsx';
 
-export default  function Home() {
-    const dispatch = useDispatch()
-    const events = useSelector((state) => state.allEvents)
-    useEffect( () => {
-         dispatch(getEvents())
-    }, [])
+export default function Home() {
+	const dispatch = useDispatch();
+	const events = useSelector((state) => state.allEvents);
+	useEffect(() => {
+		dispatch(getEvents());
+	}, []);
 
-    return(
-        <div>
-            
-               <NavBar/>
-            <Search/>
-            <EventCarousel/>
-            <ButtonFilter />
-            <CalendarEvents></CalendarEvents>
-            <div>
-            {events.length ? events.map( event => (
-                            <div key={event.ID} >
-                            <EventCard
-                            id={event.ID}
-                            name={event.Name}
-                            image={event.Image}
-                            date={event.date}
-                            price={event.Price}
-                            quantity={event.Quantity}
-                            city={event.City}
-                            location={event.Location}
-                            />
-                            </div>
-            )) 
-            : <div>
-                <Loader/>
-            </div>
-            }
-            </div>
-        </div>
-    )
+	return (
+		<div>
+			<NavBar />
+			<Search />
+			<EventCarousel />
+			<ButtonFilter />
+			<CalendarEvents></CalendarEvents>
+			<div>
+				{console.log('🐲🐲🐲 / file: Home.jsx / line 28 / events', events)};
+				{events.length ? (
+					events.map((event) => (
+						<div key={event.ID}>
+							<EventCard
+								id={event.ID}
+								name={event.Name}
+								image={event.Image[0]}
+								day={event.Day}
+								hour={event.Hour}
+								price={event.Price}
+								quantity={event.Quantity}
+								city={event.City}
+								location={event.Location}
+							/>
+						</div>
+					))
+				) : (
+					<div>
+						<Loader />
+					</div>
+				)}
+			</div>
+		</div>
+	);
 }
