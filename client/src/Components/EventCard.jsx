9a608c51a77aa4c6060cad5b7e48118ca0cdf3ee
rober-figuer <robertofigueroa.arg@gmail.com>
2022-07-23 03:35:46 -0300

    fix cart image and hover in event cards

--- client/src/Components/EventCard.jsx
+++ client/src/Components/EventCard.jsx
@@ -4,16 +4,16 @@ import styles from '../Styles/EventCard.module.css';
 
 export default function EventCard({ id, image, name, price, quantity, city, location, date, category }) {
 	return (
-		<div>
+		<div className={styles.cardContainer}>
 			<Link to={`/details/id/${id}`}>
 				<img className={styles.img} src={image} alt='img eventCard' />
 				<h4>{name}</h4>
 			</Link>
-			<p>Date: {date} </p>
-			<p>Price: ${price}</p>
-			<p>Category: {category}</p>
-			<p>City: {city}</p>
-			<p>Place: {location}</p>
+				<p>Date: {date} </p>
+				<p>Price: ${price}</p>
+				<p>Category: {category}</p>
+				<p>City: {city}</p>
+				<p>Place: {location}</p>
 		</div>
 	);
 }
