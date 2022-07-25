d5d5b928a04b262c7fef6d0197bad6fddd038a2a
PrototypeK02 <100804258+PrototypeK02@users.noreply.github.com>
2022-07-25 02:12:53 -0300

    componente pre control panel

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ import Cart from './Components/Cart';
 import LogIn from './Components/logIn';
 import Register from './Components/Register';
 import About from './Components/AboutUs'
+import Prepanel from './Components/verifyAdmin';
 
 function App() {
   return (
@@ -23,6 +24,7 @@ function App() {
       <Route path='/login' element={<LogIn/>}/>
       <Route path='/register' element={<Register/>}/>
       <Route path='/aboutUs' element={<About/>}/>
+      <Route path='/controlPanel' element={<Prepanel/>}/>
       </Routes>
     </div>
   );
