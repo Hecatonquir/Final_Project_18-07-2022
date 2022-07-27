4b694e5709591797cc6a6d78bca04159304c056e
JhonatanPG02 <jhonatan.prado.gutierrez@gmail.com>
2022-07-27 14:50:36 -0500

    boton favoritos en detalles y css paneladmin

--- client/src/Components/AdminPanel.jsx
+++ client/src/Components/AdminPanel.jsx
@@ -8,6 +8,7 @@ import { useDispatch, useSelector } from 'react-redux'
 import { deleteUserDB } from '../Redux/Actions/deleteUser'
 import {deleteEvent} from "../Redux/Actions/deleteEvent"
 import getEvents from '../Redux/Actions/getEvents'
+import styles from '../Styles/AdminPanel.module.css'
 
 
  function AdminPanel() {
@@ -36,78 +37,62 @@ function handleChange(e) {
     setUser({...userADM, [e.target.name]: e.target.value})
 }
 
-
-
-
 useEffect(() => {
     
-
     axios("http://localhost:3001/user/admin", {withCredentials: true})
     .then(response => setAdmin(true))
     .then(response => dispatch(getUsers()))
     .then(response => dispatch(getEvents()))
     .catch(error => navigate("/"))
 
-    
-    
-
-  return () => {
-  }
+  return () => {}
 }, [])
 
 
-
-    
    return (
-    <div>
+    <div className={styles.Total}>
         <div>
-        {admin && <h1>Welcome {tokenDecoded && tokenDecoded.name}</h1>}
+        {admin && <h1 className={styles.title}>Welcome {tokenDecoded && tokenDecoded.name}</h1>}
+        </div>
+        <div className={styles.container}>
+        <div className={styles.leftcolumn}>
+            <div className={styles.containerinput}>
+            {admin && <input name="username" type="text"  placeholder="Search User" value={userADM.username} onChange={(e) =>handleChange(e)} className={styles.input}></input>}
+            </div>
+
+            <div>
+            {usersBD.length && admin && usersBD.filter(el=> el.Name.toLowerCase().includes(userADM.username.toLowerCase()) && userADM.username !== "" ? el: null).slice(0,3).map((el,i) => (
+                <div key={i}>
+                    <button onClick={() => {deleteUserDB(el.Email,dispatch)
+                        setUser({username: "",posts: ""})}}>Delete User</button>
+                    <button onClick={()=> {}}>Change Role</button>
+
+                    <span>User: {el.Name} || Email: {el.Email} || Role: {el.Role}</span>
+                </div>
+            
+            ))}
+            </div>
         </div>
 
-        <div>
-       {admin && <input name="username" type="text"  placeholder="Search User" value={userADM.username} onChange={(e) =>handleChange(e)}></input>}
-       </div>
+        <div className={styles.rightcolumn}>
+            <div className={styles.containerinput}>
+            {admin && <input name="posts" type="text"  placeholder="Search Event" value={userADM.posts} onChange={(e) =>handleChange(e)} className={styles.input}></input>}
+            </div>
 
-       <div>
-       {events.length && admin && events.filter(el=> el.Name.toLowerCase().includes(userADM.posts.toLowerCase()) && userADM.posts !== "" ? el: null).slice(0,3).map((el,i) => (
-        <div key={i}>
-            <button onClick={() =>  {return deleteEvent(el.ID), setUser({username: "",posts: ""})}}>Delete Event</button>
-            <button onClick={()=>{}}>Update Event</button>
+            <div>
+            {events.length && admin && events.filter(el=> el.Name.toLowerCase().includes(userADM.posts.toLowerCase()) && userADM.posts !== "" ? el: null).slice(0,3).map((el,i) => (
+                <div key={i}>
+                    <button onClick={() =>  {return deleteEvent(el.ID), setUser({username: "",posts: ""})}}>Delete Event</button>
+                    <button onClick={()=>{}}>Update Event</button>
 
-            <span>Name: {el.Name} || Price: {el.Price} || City: {el.City} || Quantity: {el.Quantity} || Partner: </span>
+                    <span>Name: {el.Name} || Price: {el.Price} || City: {el.City} || Quantity: {el.Quantity} || Partner: </span>
+                </div>
+                ))}
+            </div>
         </div>
-     
-       ))}
-
-       </div>
-
-        <div>
-       {admin && <input name="posts" type="text"  placeholder="Search Event" value={userADM.posts} onChange={(e) =>handleChange(e)}></input>}
-       </div>
-
-       <div>
-       {usersBD.length && admin && usersBD.filter(el=> el.Name.toLowerCase().includes(userADM.username.toLowerCase()) && userADM.username !== "" ? el: null).slice(0,3).map((el,i) => (
-        <div key={i}>
-            <button onClick={() => {deleteUserDB(el.Email,dispatch)
-                 setUser({username: "",posts: ""})}}>Delete User</button>
-            <button onClick={()=> {}}>Change Role</button>
-
-            <span>User: {el.Name} || Email: {el.Email} || Role: {el.Role}</span>
         </div>
-     
-       ))}
-       </div>
-
     </div>
    )
-   
-
-
-
-  
-  
-
 }
-
 
 export default AdminPanel
