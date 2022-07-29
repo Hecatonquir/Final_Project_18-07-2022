81b8cea7b7e0876b1aabdf88c69da1ece5d251ad
Hecatonquir <maxicuentas92@hotmail.com>
2022-07-29 06:17:15 -0300

    Adding cors for deployment

--- api/src/app.js
+++ api/src/app.js
@@ -2,17 +2,17 @@ const express = require('express');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
-
-require('dotenv').config;
-
 const routes = require('./Routes/0-Routes');
-
 require('./db.js');
 
 const server = express();
+const cors = require('cors');
+
+/* require('dotenv').config; */
 
 server.name = 'API';
 
+server.use(cors());
 server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
@@ -23,7 +23,6 @@ server.use((req, res, next) => {
 	res.header('Access-Control-Allow-Credentials', 'true');
 	res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
 	res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-
 	next();
 });
 
