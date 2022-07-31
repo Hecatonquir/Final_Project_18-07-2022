ad38e7332e3607dd09c3cbad481bc527146441e9
PrototypeK02 <100804258+PrototypeK02@users.noreply.github.com>
2022-07-30 21:23:44 -0300

    testing userdetails

--- client/src/Redux/Actions/getUserDetails.js
+++ client/src/Redux/Actions/getUserDetails.js
@@ -10,7 +10,7 @@ export  const getUserDetails =  (id) => {
         try {
             console.log("hola")
 
-            let userFound = await axios.put(`http://localhost:3001/user/getUserById/${id.id}`, {withCredentials: true})
+            let userFound = await axios.put(`/user/getUserById/${id.id}`, {withCredentials: true})
 
 
             dispatch({type: USER_DETAILS, payload: userFound.data})
