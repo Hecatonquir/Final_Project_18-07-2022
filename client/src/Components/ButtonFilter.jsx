9ef93c255c5f86155c0779813185a243a32c4339
Hecatonquir <maxicuentas92@hotmail.com>
2022-08-01 09:20:09 -0300

    fixed merging problems

--- client/src/Components/ButtonFilter.jsx
+++ client/src/Components/ButtonFilter.jsx
@@ -6,21 +6,15 @@ import { Box, Button, Flex, Select, Stack } from '@chakra-ui/react';
 import swal from 'sweetalert';
 import Search from './Search';
 
-function ButtonFilter({setSearch}) {
-
-
+function ButtonFilter({ setSearch, search }) {
 	let [controlFilter, setReference] = useState({
 		city: '',
-		category: ''
-});
+		category: '',
+	});
 
-
-
-
-
-useEffect( () => {
-	newFilter()
-}, [controlFilter])
+	useEffect(() => {
+		newFilter();
+	}, [controlFilter]);
 
 	const backUp = useSelector((state) => state.eventsBackUp);
 	const allEvents = useSelector((state) => state.allEvents);
@@ -30,60 +24,65 @@ useEffect( () => {
 	let categories = Array.from(new Set(backUp.slice().map((el) => el.Category)));
 
 	function filterItems(el) {
-		const cityFound = backUp.filter((e) => e.City === el)
-		const categoryFound = backUp.filter( (e) => e.Category === el)
-		
-		if(cityFound[0]) {
-			setReference({...controlFilter, city: el} )
-			controlFilter.city = el
-		} else if (categoryFound[0]){ 
-			setReference({...controlFilter, category: el})
-			
+		const cityFound = backUp.filter((e) => e.City === el);
+		const categoryFound = backUp.filter((e) => e.Category === el);
+
+		if (cityFound[0]) {
+			setReference({ ...controlFilter, city: el });
+			controlFilter.city = el;
+		} else if (categoryFound[0]) {
+			setReference({ ...controlFilter, category: el });
 		}
 	}
 
 	function newFilter() {
-		if(controlFilter.city !== '' && controlFilter.category !== '') {
-			const bothFilter = backUp.filter( (e) => e.City === controlFilter.city && e.Category === controlFilter.category)
-			if(!bothFilter[0]) {
+		if (controlFilter.city !== '' && controlFilter.category !== '') {
+			const bothFilter = backUp.filter(
+				(e) => e.City === controlFilter.city && e.Category === controlFilter.category
+			);
+			if (!bothFilter[0]) {
 				document.getElementById('city').value = 'City';
 				document.getElementById('cat').value = 'Categories';
-				setReference((controlFilter = {
-					city: '',
-					category: ''
-				}))
-				dispatch({type: SHOW_EVENTS_USER, payload: backUp})
-				return swal('No match for this combination',{icon:"warning"});
+				setReference(
+					(controlFilter = {
+						city: '',
+						category: '',
+					})
+				);
+				dispatch({ type: SHOW_EVENTS_USER, payload: backUp });
+				return swal('No match for this combination', { icon: 'warning' });
 			}
-			return dispatch({type: SHOW_EVENTS_USER, payload: bothFilter})
+			return dispatch({ type: SHOW_EVENTS_USER, payload: bothFilter });
 		} else if (controlFilter.city !== '' && controlFilter.category === '') {
-			const cityFilter = backUp.filter((e) => e.City === controlFilter.city)
-			return dispatch({type: SHOW_EVENTS_USER, payload: cityFilter})
+			const cityFilter = backUp.filter((e) => e.City === controlFilter.city);
+			return dispatch({ type: SHOW_EVENTS_USER, payload: cityFilter });
 		} else if (controlFilter.city === '' && controlFilter.category !== '') {
-			const categoryFilter = backUp.filter((e) => e.Category === controlFilter.category)
-			return dispatch({type: SHOW_EVENTS_USER, payload: categoryFilter})
+			const categoryFilter = backUp.filter((e) => e.Category === controlFilter.category);
+			return dispatch({ type: SHOW_EVENTS_USER, payload: categoryFilter });
 		}
 	}
-		
 
 	let handleSelect = (e) => {
 		return filterItems(e.target.value);
 	};
-	
 
 	return (
 		<>
 			<Stack spacing={10} marginTop={6} marginBottom={6}>
-				<Flex justifyContent='space-around' width='100%' >
-				<Search/>
+				<Flex justifyContent='space-around' width='100%'>
+					<div className={styles.search}>
+						<Search search={search} setSearch={setSearch} />
+					</div>
 					<Select
 						variant='flushed'
-						marginLeft="5rem"
+						marginLeft='5rem'
 						width='30%'
 						onChange={(e) => handleSelect(e)}
 						color='gray'
 						id='city'>
-						<option value='City' hidden >Filter By City</option>
+						<option value='City' hidden>
+							Filter By City
+						</option>
 						{cities.map((el, i) => (
 							<option key={i} onClick={() => filterItems(el)}>
 								{el}
@@ -93,36 +92,38 @@ useEffect( () => {
 
 					<Select
 						variant='flushed'
-						marginLeft="5rem"
-						marginRight="5rem"
+						marginLeft='5rem'
+						marginRight='5rem'
 						width='30%'
 						onChange={(e) => handleSelect(e)}
 						color='gray'
 						id='cat'>
-						<option value='Categories' hidden >Filter By Categories</option>
+						<option value='Categories' hidden>
+							Filter By Categories
+						</option>
 						{categories.map((el, i) => (
 							<option key={i} onClick={() => filterItems(el)}>
 								{el}
 							</option>
 						))}
 					</Select>
-                  
+
 					<Box>
 						<Button
 							className={styles.Button2}
 							onClick={() => {
-									document.getElementById('city').value = 'City';
-									document.getElementById('cat').value = 'Categories'
+								document.getElementById('city').value = 'City';
+								document.getElementById('cat').value = 'Categories';
 								return (
-					
-									setReference((controlFilter = {
-										city: '',
-										category: ''
-									})),
-									setSearch(""),
+									setReference(
+										(controlFilter = {
+											city: '',
+											category: '',
+										})
+									),
+									setSearch(''),
 									dispatch({ type: GET_EVENTS, payload: backUp }),
 									dispatch({ type: SHOW_EVENTS_USER, payload: backUp.slice(0, 15) })
-									
 								);
 							}}
 							bg='#f4a69a'>
--- client/src/Components/NavBar.jsx
+++ client/src/Components/NavBar.jsx
@@ -1,10 +1,10 @@
-import React, {useState,useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { Link, useNavigate } from 'react-router-dom';
 import styles from '../Styles/NavBar.module.css';
 import { useAuth0 } from '@auth0/auth0-react';
 import imgcarrito from '../Media/carri.png';
-import imglogo from '../Media/logo2.png'
+import imglogo from '../Media/logo2.png';
 import { isExpired, decodeToken } from 'react-jwt';
 import logOut from '../Redux/Actions/logOut';
 import Search from './Search.jsx';
@@ -13,12 +13,9 @@ import { CLEAR_CART, UPDATE_STATE_TRUE } from '../Redux/ActionTypes/actiontypes'
 
 function NavBar() {
 	let { isAuthenticated, logout, user } = useAuth0();
-	let token = document.cookie
-		.split(';')[0]
-	let token1 = 
-		token
-		.split('=')[1]
-	
+	let token = document.cookie.split(';')[0];
+	let token1 = token.split('=')[1];
+
 	//console.log(document.cookie);
 	let tokenDecoded = decodeToken(token1);
 	let dispatch = useDispatch();
@@ -27,13 +24,6 @@ function NavBar() {
 	const events = useSelector((state) => state.showToUser);
 	const active = useSelector((state) => state.loginState);
 	let count = cart ? cart.length : null;
-
-	let [search, setSearch] = useState("")
-
-	
-
-	
-	
 
 	//  return (
 	//     <Box marginBottom={6} padding={2} bgGradient='linear(to-b, #a28748, #6c5727)'>
@@ -60,7 +50,6 @@ function NavBar() {
 	//                 </Link>
 	//               )}
 	//             </Box>
-
 
 	//             {isAuthenticated ? (
 	//               <Box>
@@ -93,63 +82,80 @@ function NavBar() {
 	//   )
 
 	return (
-		<Box marginBottom={6} padding={2} bgGradient='linear(to-b, blue.700, green.500)'>
-				<Flex alignItems='center' width='100%' justifyContent='space-between' margin='0.5rem' position='sticky' top='0px' zIndex='10'>
-					<Box>
-						<Heading as='h4' color='white'>
-						<Image src={imglogo} alt='img logo' width='6.5rem' height='4rem' marginLeft='1rem' marginTop='0rem'/>
-						</Heading>
-					</Box>
-					<Box>
-						{!token ? (
-							<Box>
-								<Link to='/login'>
-									<Button bg='#f4a69a' >Login/Sign Up</Button>
-								</Link>
-							</Box>
-						) : (
-							<Box></Box>
-						)}
+		<Box marginBottom={4} padding={1} bgGradient='linear(to-b, blue.700, green.500)'>
+			<Flex
+				alignItems='center'
+				width='100%'
+				justifyContent='space-between'
+				margin='0.5rem'
+				position='sticky'
+				top='0px'
+				zIndex='10'>
+				<Box>
+					<Heading as='h4' color='white'>
+						<Image
+							src={imglogo}
+							alt='img logo'
+							width='6.5rem'
+							height='4rem'
+							marginLeft='1rem'
+							marginTop='0rem'
+						/>
+					</Heading>
+				</Box>
+				<Box>
+					{!token ? (
+						<Box>
+							<Link to='/login'>
+								<Button bg='#f4a69a'>Login/Sign Up</Button>
+							</Link>
+						</Box>
+					) : (
+						<Box></Box>
+					)}
 
-						{token && (
-							<Button
-								bg='#f4a69a'
-								className={styles.Button}
-								color='white'
-								onClick={() => { dispatch({type: CLEAR_CART}); return logOut(dispatch)}}>
-								<Text>Log Out</Text>
-							</Button>
-						)}
+					{token && (
+						<Button
+							bg='#f4a69a'
+							className={styles.Button}
+							color='white'
+							onClick={() => {
+								dispatch({ type: CLEAR_CART });
+								return logOut(dispatch);
+							}}>
+							<Text>Log Out</Text>
+						</Button>
+					)}
 
-						{/* {token && tokenDecoded.role === 'Partner' && active && (
+					{/* {token && tokenDecoded.role === 'Partner' && active && (
 							<Link to='/createEvent'>
 								<button className={styles.Button}>Create an Event</button>
 							</Link>
 						)} */}
 
-						{token && tokenDecoded && tokenDecoded.role === 'Partner' && active && (
-							<Link to='/createEvent'>
-								<Button bg='#f4a69a' >Create an Event</Button>
-							</Link>
-						)}
-					</Box>
-					{token && active && (
-						<Box>
-							<Link to='/profile'>
-								<Button bg='#f4a69a'>
-									<Text>Profile</Text>
-								</Button>
-							</Link>
-						</Box>
+					{token && tokenDecoded && tokenDecoded.role === 'Partner' && active && (
+						<Link to='/createEvent'>
+							<Button bg='#f4a69a'>Create an Event</Button>
+						</Link>
 					)}
-					
+				</Box>
+				{token && active && (
 					<Box>
-						<Link to={token? '/cart' : '/login'}>
-							<Image src={imgcarrito} alt='img carrito' width='3rem' marginRight='1rem' />
-							<span className={styles.count}>{count}</span>
+						<Link to='/profile'>
+							<Button bg='#f4a69a'>
+								<Text>Profile</Text>
+							</Button>
 						</Link>
 					</Box>
-				</Flex>
+				)}
+
+				<Box>
+					<Link to={token ? '/cart' : '/login'}>
+						<Image src={imgcarrito} alt='img carrito' width='3rem' marginRight='1rem' />
+						<span className={styles.count}>{count}</span>
+					</Link>
+				</Box>
+			</Flex>
 		</Box>
 	);
 }
