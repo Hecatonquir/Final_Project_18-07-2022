71dd74644c8107dcfb689992cd380a175da3474e
Sol <solabigail00@hotmail.com>
2022-08-03 11:48:34 -0300

    Css AddEvent

--- client/src/Components/Nav.jsx
+++ client/src/Components/Nav.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { background, Box, Button } from "@chakra-ui/react";
+import { Box, Button } from "@chakra-ui/react";
 
 function Nav() {
   return (
     <>
-      <Box bg='#222831'>
+      <Box bg="#222831">
         <Link to="/">
-        <Button margin={2} bg='#FD7014' color='#EEEEEE'
-        _hover={{bg:'#EEEEEE', color:'black'}}>
-         
+          <Button
+            margin={2}
+            bg="#FD7014"
+            color="#EEEEEE"
+            _hover={{ bg: "#EEEEEE", color: "black" }}
+          >
             Back
           </Button>
         </Link>
