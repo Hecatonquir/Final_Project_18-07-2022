2ebee04fcfff635ca61e2f1ea6100501bb453a4c
Sol <solabigail00@hotmail.com>
2022-08-05 22:42:55 -0300

    Css Cart

--- client/src/Components/CartItem.jsx
+++ client/src/Components/CartItem.jsx
@@ -5,7 +5,7 @@ import { removeCart } from '../Redux/Actions/removeFromCart';
 import { removeAllCart } from '../Redux/Actions/removeAllFromCart';
 import styles from '../Styles/CartItem.module.css';
 import imgdelete from '../Media/delete.png';
-import { Box, Heading, Image, Button, Text, Flex, Center} from "@chakra-ui/react";
+import { Box, Heading, Image, Button, Text, Flex, Center, useMediaQuery} from "@chakra-ui/react";
 import { decodeToken } from 'react-jwt';
 import { updateCart } from '../Redux/Actions/updateCart';
 
@@ -38,31 +38,34 @@ export default function CardItem({id, name, image, price, purchasedItem, quantit
         dispatch(updateCart(tokenDecoded.id))
     }
 
+      //Responsive
+      const [smallScreen] = useMediaQuery("(min-width: 1249px)");
+      // w={!smallScreen ? "60%" : "45%"}
+
     return(
         <Center>
             <div className={styles.container}>
-            <Box marginBottom={8} bg='white' borderRadius="10px" border='0px solid red'>
-            <Box width='80vw'>
+            <Box marginBottom={8} bg='#EEEEEE' borderRadius="10px" border='0px solid red'>
+            <Box w={!smallScreen ? "90vw" : "80vw"}>
                 <Flex justifyContent='space-around' alignItems='center'>
-                <Image src={image} alt='img event'width='150px' height='150px'/>
-                <Heading as='h4' fontSize='1.5em'>{name}</Heading>
+                <Image src={image} alt='img event' w={!smallScreen ? "75px" : "150px"} h={!smallScreen ? "75px" : "150px"}/>
+                <Heading as='h4' fontSize={!smallScreen ? ".8em" : "1.5em"}>{name}</Heading>
                 <Box>
                     <Flex flexDirection='column' alignItems='center'>
-                        <Heading as='h5' fontSize='1.5em' marginBottom={4}>Quantity Item</Heading>
+                        <Heading as='h5' fontSize={!smallScreen ? ".7em" : "1.5em"} marginBottom={4}>Quantity Item</Heading>
                     </Flex>
                 <Box >
                 <Flex alignItems='center'>
-                    <Button width='30px' height='30px' borderRadius='5px' bg='#229ddb' color='white' onClick={() => hundleRemoveItem()} disabled={purchasedItem === 1 ? true : false }>-</Button>
-                    <Text margin='0 .5em' fontSize='1.5em'>{purchasedItem}</Text>
-                    <Button width='30px' height='30px' borderRadius='5px' bg='#229ddb' color='white' onClick={() => hundleAddItem()}>+</Button>
+                    <Button w={!smallScreen ? "15px" : "30px"} h={!smallScreen ? "15px" : "30px"} borderRadius='5px' bg='#229ddb' color='white' onClick={() => hundleRemoveItem()} disabled={purchasedItem === 1 ? true : false }>-</Button>
+                    <Text margin='0 .5em' fontSize={!smallScreen ? ".7em" : "1.5em"}>{purchasedItem}</Text>
+                    <Button w={!smallScreen ? "15px" : "30px"} h={!smallScreen ? "15px" : "30px"} borderRadius='5px' bg='#229ddb' color='white' onClick={() => hundleAddItem()}>+</Button>
                 </Flex>
                 </Box>
                 </Box>
             <Box>
-                <Heading as='h5' fontSize='1.5em'>Price</Heading>
-                <Text textAlign='center'>${price*purchasedItem}</Text>
+                <Heading as='h5' fontSize={!smallScreen ? ".7em" : "1.5em"}>Price</Heading>
+                <Text textAlign='center' fontSize={!smallScreen ? ".6em" : "1em"}>${price*purchasedItem}</Text>
             </Box>
-            {/* <Button bg='#e94646' color='white' borderRadius='40px' onClick={() => hundleDelete()}>Delete</Button> */}
             <img src={imgdelete} alt='not imgdelete' onClick={() => hundleDelete()} className={styles.imgdelete}/>
                 </Flex>
             </Box>
