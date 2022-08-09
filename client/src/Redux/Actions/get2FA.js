89c65ec2cf5279cdbbbc18a1d10749c7de11cd7f
rober-figuer <robertofigueroa.arg@gmail.com>
2022-08-09 03:26:06 -0300

    funcionalidad de mailing para envio de token 2FA

--- client/src/Redux/Actions/get2FA.js
+++ client/src/Redux/Actions/get2FA.js
@@ -1,20 +1,31 @@
-import axios from "axios"
-import swal from "sweetalert"
+import axios from "axios";
+import swal from "sweetalert";
+import emailjs from '@emailjs/browser';
 
 
-export default async function get2FA(id) {
+export default async function get2FA(id, userEmail) {
 
-    try {
-        
+    // ID para servicio de mailing
+    const SERVICE_ID = 'service_y1hcd5d' // cuenta de google: customers.mainstage@gmail.com
+    const TEMPLATE_ID = 'template_p780upm'  // Two factor Authentication template
+    const PUBLIC_KEY = '5_m-uTWTRGUxgzYAN'
     
-
-    let data = await axios.put("/user/get2fa", {id: id})
+    try {
+        let data = await axios.put("/user/get2fa", {id: id})
         
-    swal({text: `${data.data}. Please keep it safe`, icon: "success"})
+        /////////////////////////Envio de mail de token 2FA //////////////////////////////
+        const templateParams = { token2FA: data.data,
+                                 userEmail: userEmail };
+        console.log(templateParams)
+    
+        emailjs.send( SERVICE_ID, TEMPLATE_ID, templateParams, PUBLIC_KEY )
+            .then((result) => { console.log(result.text); } , (error) => { console.log(error.text); });
+        /////////////////////////////////////////////////////////////////////////////////////////////
+
+        swal({text: `${data.data}. Please keep it safe`, icon: "success"})
 
     }catch (error) {
         swal({text: error.response.data, icon:"error"})
-        
     }
 
 
