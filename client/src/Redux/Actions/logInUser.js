d35f5b0783c5e65a5b0f5ce9b1e12540c650ddeb
Hecatonquir <maxicuentas92@hotmail.com>
2022-11-10 07:23:24 -0300

    fix

--- client/src/Redux/Actions/logInUser.js
+++ client/src/Redux/Actions/logInUser.js
@@ -1,6 +1,6 @@
-import axios from "axios";
-import { LOAD_CART, UPDATE_STATE_TRUE } from "../ActionTypes/actiontypes";
-import swal from "sweetalert";
+import axios from 'axios';
+import { LOAD_CART, UPDATE_STATE_TRUE } from '../ActionTypes/actiontypes';
+import swal from 'sweetalert';
 
 export async function logInUser(payload, navigate, dispatch, setCookie) {
 	try {
@@ -9,23 +9,22 @@ export async function logInUser(payload, navigate, dispatch, setCookie) {
 		});
 
 		swal({
-			title: "Login Success",
-			text: "Redirecting...",
-			icon: "success",
+			title: 'Login Success',
+			text: 'Redirecting...',
+			icon: 'success',
 			timer: 2000,
 			buttons: false,
 		});
 
 		setTimeout(() => {
-			navigate("/");
-			setCookie("access-control", user.data, { path: "/" });
+			navigate('/');
+			setCookie('access-control', user.data, { path: '/' });
 		}, 2000);
 	} catch (error) {
-		console.log("🐲🐲🐲 / file: logInUser.js / line 24 / error:\n", error);
-
-		swal({
-			title: error.response.data,
-			icon: "error",
+		//console.log('🐲🐲🐲 / file: logInUser.js / line 24 / error:\n', error);
+		swal('This account has been banned.', {
+			title: "Couldn't logg in",
+			icon: 'error',
 			timer: 10000,
 			buttons: false,
 		});
